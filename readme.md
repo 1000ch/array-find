b0baac35ed5e3fb2affeb31a7cf768e5b1fb847a
1000ch <shogo.sensui@gmail.com>
2014-06-01 22:41:56 +0900

    add tests

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# array-find
+# array-find [![Build Status](https://travis-ci.org/1000ch/array-find.svg?branch=master)](https://travis-ci.org/1000ch/array-find)
 
 [![testling badge](https://ci.testling.com/1000ch/array-find.png)](https://ci.testling.com/1000ch/array-find)
--- test/find.js
+++ test/find.js
@@ -1,3 +1,30 @@
 var find = require('../');
 var test = require('tape');
 
+function isPrimaryNumber(number, index, array) {
+  var start = 2;
+  while (start <= Math.sqrt(number)) {
+    if (number % start++ < 1) {
+      return false;
+    }
+  }
+  return (number > 1);
+}
+
+test('return undefined', function (t) {
+
+  t.plan(1);
+  t.equal(
+    find([4, 6, 8, 10], isPrimaryNumber),
+    undefined
+  );
+});
+
+test('return 5', function (t) {
+
+  t.plan(1);
+  t.equal(
+    find([4, 5, 8, 10], isPrimaryNumber),
+    5
+  );
+});
